0d6d8a17aa43622b58807ece1aefa623f085be59
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-05-12 22:51:03 +0100

    Add localhost network in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,10 @@ module.exports = {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337
     }
   },
   etherscan: {
