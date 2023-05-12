5627e3caf95f8488a8a39e5234a2323787d28b76
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-05-12 15:55:06 +0100

    Replace Lock.sol with SimpleStorage.sol

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 public favouriteNumber;
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    struct People {
+       uint256 favouriteNumber;
+       string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+          people.push(People(_favouriteNumber, _name));
+          nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
