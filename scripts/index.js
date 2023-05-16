2c37d1203574da4fae1e80898d521bfdfb885569
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-05-16 21:20:59 +0100

    WIP: just testing with index.js

---
+++ scripts/index.js
@@ -0,0 +1,89 @@
+const { ethers } = require('ethers');
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
+const address = '0x99E7A010d8914b71ea486b6469364A3C0B265e7E';
+const abi = [
+	{
+		inputs: [{ internalType: 'uint256', name: '_id', type: 'uint256' }],
+		name: 'attend',
+		outputs: [],
+		stateMutability: 'payable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{ internalType: 'address', name: '_owner', type: 'address' },
+			{ internalType: 'string', name: '_name', type: 'string' },
+			{ internalType: 'string', name: '_description', type: 'string' },
+			{ internalType: 'string', name: '_location', type: 'string' },
+			{ internalType: 'uint256', name: '_moment', type: 'uint256' },
+		],
+		name: 'create',
+		outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		name: 'events',
+		outputs: [
+			{ internalType: 'address', name: 'owner', type: 'address' },
+			{ internalType: 'string', name: 'name', type: 'string' },
+			{ internalType: 'string', name: 'description', type: 'string' },
+			{ internalType: 'string', name: 'location', type: 'string' },
+			{ internalType: 'uint256', name: 'moment', type: 'uint256' },
+			{ internalType: 'uint256', name: 'totalPayed', type: 'uint256' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'eventsCount',
+		outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [{ internalType: 'uint256', name: '_id', type: 'uint256' }],
+		name: 'getAttendees',
+		outputs: [
+			{ internalType: 'address[]', name: '', type: 'address[]' },
+			{ internalType: 'uint256[]', name: '', type: 'uint256[]' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'getEvents',
+		outputs: [
+			{
+				components: [
+					{ internalType: 'address', name: 'owner', type: 'address' },
+					{ internalType: 'string', name: 'name', type: 'string' },
+					{ internalType: 'string', name: 'description', type: 'string' },
+					{ internalType: 'string', name: 'location', type: 'string' },
+					{ internalType: 'uint256', name: 'moment', type: 'uint256' },
+					{ internalType: 'address[]', name: 'attendees', type: 'address[]' },
+					{ internalType: 'uint256[]', name: 'attendessPayments', type: 'uint256[]' },
+					{ internalType: 'uint256', name: 'totalPayed', type: 'uint256' },
+				],
+				internalType: 'struct EventContract.Event[]',
+				name: '',
+				type: 'tuple[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+];
+const contract = new ethers.Contract(address, abi, provider);
+
+const main = async () => {
+	const allEvents = await contract.getEvents();
+
+	console.log(allEvents);
+};
+
+main();
