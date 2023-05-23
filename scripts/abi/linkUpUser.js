074fd7a83db717e1bd43abbebad8e2213055719a
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-05-23 19:21:57 +0100

    Add LinkUpUser contract and abi.

---
+++ scripts/abi/linkUpUser.js
@@ -0,0 +1,31 @@
+module.exports = [
+	{
+		inputs: [],
+		name: 'count',
+		outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{ internalType: 'uint256', name: '_link_up_id', type: 'uint256' },
+			{ internalType: 'uint256', name: '_user_id', type: 'uint256' },
+			{ internalType: 'string', name: '_response', type: 'string' },
+		],
+		name: 'create',
+		outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		name: 'linkUpUsers',
+		outputs: [
+			{ internalType: 'uint256', name: 'link_up_id', type: 'uint256' },
+			{ internalType: 'uint256', name: 'user_id', type: 'uint256' },
+			{ internalType: 'string', name: 'response', type: 'string' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+];
